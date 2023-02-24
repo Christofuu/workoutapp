d653cab171550505aa4d2afd4eb340e9bf90dbae
Christofuu <chrisdove1337@gmail.com>
2023-02-24 15:25:59 -0800

    add login css

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -19,42 +19,42 @@ export default function Login() {
         console.log(data.username, data.password);
     }
 
-    const registerHandler = e => {
-
-    }
-
     return(
     <div className={styles.login_wrapper}>
         <h1>LiftBuddy</h1>
             {isRegistering ? (
                 <div className={styles.login_menu}>
-                <h2>REGUISTER</h2>
+                <h2>Register an account</h2>
                 <form onSubmit={submitHandler}>
                     <label>
                         Username: 
-                        <input type="text" name="username" value={username} onChange={changeHandler} />
                     </label>
+                        <input type="text" name="username" value={username} onChange={changeHandler} />
                     <label>
                         Password: 
-                        <input type="text" name="password" value={password} onChange={changeHandler} />
                     </label>
+                        <input type="text" name="password" value={password} onChange={changeHandler} />
+                    <label>
+                        Confirm Password:
+                    </label>
+                        <input type="text" name="password" value={password} onChange={changeHandler} />
                     <input type="submit" value="Submit" />
-                    <input type="button" value="ROGIN! " onClick={()=>setIsRegistering(false)}/>
+                    <input type="button" value="Back to login" onClick={()=>setIsRegistering(false)}/>
                 </form>
             </div>
             ) : (
             <div className={styles.login_menu}>
-                <h2>LOGIN.</h2>
+                <h2>Login</h2>
                 <form onSubmit={submitHandler}>
                     <label>
                         Username: 
-                        <input type="text" name="username" value={username} onChange={changeHandler} />
                     </label>
+                        <input type="text" name="username" value={username} onChange={changeHandler} />
                     <label>
                         Password: 
-                        <input type="text" name="password" value={password} onChange={changeHandler} />
                     </label>
-                    <input type="submit" value="Submit" />
+                        <input type="text" name="password" value={password} onChange={changeHandler} />
+                    <input type="submit" value="Login" />
                     <input type="button" value="Register " onClick={()=>setIsRegistering(true)}/>
                 </form>
             </div>)}
