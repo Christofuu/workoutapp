61ae9b4883711416619ca557d63889d82b209c0d
Christofuu <chrisdove1337@gmail.com>
2023-02-24 13:05:16 -0800

    add login form

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,15 +1,39 @@
+import { useState } from "react";
 import styles from "../styles/login.module.css";
 
 export default function Login() {
+    const [data, setData] = useState({
+        username:"",
+        password:""
+    });
+
+    const {username, password} = data;
+
+    const changeHandler = e => {
+        setData({...data, [e.target.name]:[e.target.value]})
+    }
+
+    const submitHandler = e => {
+        // e.preventDefault();
+        console.log(data);
+    }
+
     return(
     <div className={styles.login_wrapper}>
         <h1>LiftBuddy</h1>
             <div className={styles.login_menu}>
-                <h2>To use LiftBuddy, sign in with...</h2>
-                <button >Google</button>
-                <button >Facebook</button>
-                <button >Discord</button>
-                <button >Email</button>
+                <h2>To use LiftBuddy, create an account.</h2>
+                <form onSubmit={submitHandler}>
+                    <label>
+                        Username: 
+                        <input type="text" name="username" value={username} onChange={changeHandler} />
+                    </label>
+                    <label>
+                        Password: 
+                        <input type="text" name="password" value={password} onChange={changeHandler} />
+                    </label>
+                    <input type="submit" value="Submit" />
+                </form>
             </div>
         </div>
     );
